4511130cd1846e985438dc6182829ad6aac6f148
KyleLimon <caiohyuga26@gmail.com>
2024-04-22 15:42:46 -0300

    Atv3

--- ATV-03_CaioLima/cadastrar.py
+++ ATV-03_CaioLima/cadastrar.py
@@ -7,3 +7,10 @@ class Cadastrar:
 
     def adicionarAluno (self, aluno):
         self.alunos.append(aluno)
+
+    
+    def get_aluno(self):
+        return self.__aluno
+      
+    def set_aluno(self, aluno):
+        self.__aluno = aluno
--- ATV-03_CaioLima/main.py
+++ ATV-03_CaioLima/main.py
@@ -1,4 +1,11 @@
 from aluno import *
 from professor import *
 from cadastrar import *
+from diretor import *
+from coordenador import *
 
+
+def cadastrarAluno():
+    if Diretor or Coordenador:
+       return Cadastrar.adicionarAluno()
+    
