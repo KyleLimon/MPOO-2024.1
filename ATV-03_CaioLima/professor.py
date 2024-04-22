b095e0df59ad7ba2062aa6bfebaf8800df301c9f
KyleLimon <caiohyuga26@gmail.com>
2024-04-22 17:18:23 -0300

    atv3

--- ATV-03_CaioLima/professor.py
+++ ATV-03_CaioLima/professor.py
@@ -6,3 +6,4 @@ class Professor(Servidor):
     def __init__ (self,cargo, nome, endereco, formacao):
         super().__init__(self, cargo, nome, endereco)
         self.formacao = formacao
+        
